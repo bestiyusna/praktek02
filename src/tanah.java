33626182481a3bae9bfb4a815865af48cbcd66a8
bestiyusna <33086600+bestiyusna@users.noreply.github.com>
2018-03-29 13:40:41 +0800

    membuat atribut dan behavior

--- src/tanah.java
+++ src/tanah.java
@@ -1,4 +1,12 @@
 
   public class tanah {
+      double lebar;
+      double panjang;
+      
+      void cetakInfo(){
+          System.out.println("Panjang       :"+lebar);
+          System.out.println("Panjang       :"+panjang);
+          
+      }
     
 }
