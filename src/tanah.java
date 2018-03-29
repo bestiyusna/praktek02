db88af544986b0c8c97dc4f8c87f857de5f19952
bestiyusna <33086600+bestiyusna@users.noreply.github.com>
2018-03-29 13:51:45 +0800

    method dengan nilai balik

--- src/tanah.java
+++ src/tanah.java
@@ -7,6 +7,14 @@
           System.out.println("Panjang       :"+lebar);
           System.out.println("Panjang       :"+panjang);
           
+          
       }
+      
+      
+      double hitungLuas (){
+          double luas;
+          luas=panjang*lebar;
+          return luas;
     
 }
+  }
